32af987447d9985d1b358cca3b6cd0346a7299bc
Meezzi <alswl-3016@naver.com>
2025-05-13 20:59:31 +0900

    feat: MovieResponseDto, MovieDto 기본값 처리 추가

--- lib/feature/home/data/dtos/movie_response_dto.dart
+++ lib/feature/home/data/dtos/movie_response_dto.dart
@@ -1,51 +1,109 @@
 class MovieResponseDto {
-  bool? adult;
-  String? backdropPath;
-  List<int>? genreIds;
-  int? id;
-  String? originalLanguage;
-  String? originalTitle;
-  String? overview;
-  double? popularity;
-  String? posterPath;
-  String? releaseDate;
-  String? title;
-  bool? video;
-  double? voteAverage;
-  int? voteCount;
+  final DateRangeDto dates;
+  final int page;
+  final List<MovieDto> results;
+  final int totalPages;
+  final int totalResults;
 
   MovieResponseDto({
-    this.adult,
-    this.backdropPath,
-    this.genreIds,
-    this.id,
-    this.originalLanguage,
-    this.originalTitle,
-    this.overview,
-    this.popularity,
-    this.posterPath,
-    this.releaseDate,
-    this.title,
-    this.video,
-    this.voteAverage,
-    this.voteCount,
+    required this.dates,
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
   });
 
-  MovieResponseDto.fromJson(Map<String, dynamic> json) {
-    adult = json['adult'];
-    backdropPath = json['backdrop_path'];
-    genreIds = json['genre_ids']?.cast<int>();
-    id = json['id'];
-    originalLanguage = json['original_language'];
-    originalTitle = json['original_title'];
-    overview = json['overview'];
-    popularity = (json['popularity'] as num?)?.toDouble();
-    posterPath = json['poster_path'];
-    releaseDate = json['release_date'];
-    title = json['title'];
-    video = json['video'];
-    voteAverage = (json['vote_average'] as num?)?.toDouble();
-    voteCount = json['vote_count'];
+  // API에서 기본값이 설정되지 않는 것만 기본값 설정
+  factory MovieResponseDto.fromJson(Map<String, dynamic> json) {
+    return MovieResponseDto(
+      dates:
+          json['dates'] != null
+              ? DateRangeDto.fromJson(json['dates'])
+              : DateRangeDto(minimum: '', maximum: ''),
+      page: json['page'],
+      results:
+          (json['results'] as List<dynamic>?)
+              ?.map((e) => MovieDto.fromJson(e))
+              .toList() ??
+          [],
+      totalPages: json['total_pages'],
+      totalResults: json['total_results'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'page': page,
+      'results': results.map((e) => e.toJson()).toList(),
+      'total_pages': totalPages,
+      'total_results': totalResults,
+    };
+  }
+}
+
+class DateRangeDto {
+  final String minimum;
+  final String maximum;
+
+  DateRangeDto({required this.minimum, required this.maximum});
+
+  factory DateRangeDto.fromJson(Map<String, dynamic> json) {
+    return DateRangeDto(minimum: json['minimum'], maximum: json['maximum']);
+  }
+
+  Map<String, dynamic> toJson() => {'minimum': minimum, 'maximum': maximum};
+}
+
+class MovieDto {
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  MovieDto({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  factory MovieDto.fromJson(Map<String, dynamic> json) {
+    return MovieDto(
+      adult: json['adult'],
+      backdropPath: json['backdrop_path'] ?? '',
+      genreIds: List<int>.from(json['genre_ids'] ?? []),
+      id: json['id'],
+      originalLanguage: json['original_language'] ?? '',
+      originalTitle: json['original_title'] ?? '',
+      overview: json['overview'] ?? '',
+      popularity: json['popularity'],
+      posterPath: json['poster_path'] ?? '',
+      releaseDate: json['release_date'] ?? '',
+      title: json['title'] ?? '',
+      video: json['video'],
+      voteAverage: json['vote_average'],
+      voteCount: json['vote_count'],
+    );
   }
 
   Map<String, dynamic> toJson() {
