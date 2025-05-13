d436029fd9edede7329822d9dee6d56f02f54b21
Meezzi <alswl-3016@naver.com>
2025-05-13 23:09:44 +0900

    feat: RemoteMovieRepository 에서 영화 상세 정보 가져오기 구현

---
+++ lib/feature/home/data/repositories/remote_movie_repository.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_flicksy/feature/home/data/services/remote_movie_service.dart';
+import 'package:flutter_flicksy/feature/home/domain/entities/movie_detail.dart';
+import 'package:flutter_flicksy/feature/home/domain/repositories/movie_repository.dart';
+
+class RemoteMovieRepository implements MovieRepository {
+  RemoteMovieRepository(this._service);
+
+  final RemoteMovieService _service;
+
+  @override
+  Future<MovieDetail?> fetchMovieDetail(int id) async {
+    final result = await _service.fetchMovieDetail(id);
+
+    return MovieDetail(
+      budget: result!.budget,
+      genres: result.genres.map((e) => e.name).toList(),
+      id: id,
+      productionCompanyLogos:
+          result.productionCompanies.map((e) => e.name).toList(),
+      overview: result.overview,
+      popularity: result.popularity,
+      releaseDate: DateTime.tryParse(result.releaseDate) ?? DateTime(1900),
+      revenue: result.revenue,
+      runtime: result.runtime,
+      tagline: result.tagline,
+      title: result.title,
+      voteAverage: result.voteAverage,
+      voteCount: result.voteCount,
+    );
+  }
+}
