5a8eea4c1a33b8811c8adbe0ed95a910b384266f
Meezzi <alswl-3016@naver.com>
2025-05-14 14:05:47 +0900

    feat: ProviderScope으로 MyApp 감싸기

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_flicksy/feature/home/presentation/home/home_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   await dotenv.load();
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
