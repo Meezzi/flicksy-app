0d66496b5a1cc80429966e716d84730eac75de2e
Meezzi <alswl-3016@naver.com>
2025-05-14 14:05:13 +0900

    feat: remote movie service에서 헤더 추가

--- lib/feature/home/data/providers/repository_providers.dart
+++ lib/feature/home/data/providers/repository_providers.dart
@@ -1,12 +1,18 @@
 // Service
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_flicksy/feature/home/data/repositories/remote_movie_repository.dart';
 import 'package:flutter_flicksy/feature/home/data/services/remote_movie_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Service
 final _remoteMovieServiceProvider = Provider<RemoteMovieService>((ref) {
-  final dio = Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/3/movie'));
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3/movie',
+      headers: {'Authorization': 'Bearer ${dotenv.env['TMDB_API_KEY']}'},
+    ),
+  );
 
   return RemoteMovieService(dio: dio);
 });
