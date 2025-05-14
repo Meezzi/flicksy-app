c0bf5a5ef1bef4538ea153537b079e4e90c1e633
Meezzi <alswl-3016@naver.com>
2025-05-14 22:18:55 +0900

    feat: fetchMovies 메서드 분리 및 업데이트 처리

--- lib/feature/home/presentation/viewmodels/home_view_model.dart
+++ lib/feature/home/presentation/viewmodels/home_view_model.dart
@@ -5,6 +5,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class HomeViewModel extends AsyncNotifier<HomeState> {
   @override
   Future<HomeState> build() async {
+    return await _fetchMovies();
+  }
+
+  Future<void> fetchMovies() async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() async => await _fetchMovies());
+  }
+
+  Future<HomeState> _fetchMovies() async {
     try {
       final nowPlaying =
           await ref.read(fetchMoviesNowPlayingUsecaseProvider).execute() ?? [];
