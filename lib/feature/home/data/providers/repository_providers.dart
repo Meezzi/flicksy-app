dcc7ab076bc89b9919c0942e6b00511422dbb4a5
Meezzi <alswl-3016@naver.com>
2025-05-14 13:15:39 +0900

    feat: remoteMovieRepositoryProvider 추가

--- lib/feature/home/data/providers/repository_providers.dart
+++ lib/feature/home/data/providers/repository_providers.dart
@@ -1,5 +1,6 @@
 // Service
 import 'package:dio/dio.dart';
+import 'package:flutter_flicksy/feature/home/data/repositories/remote_movie_repository.dart';
 import 'package:flutter_flicksy/feature/home/data/services/remote_movie_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,4 +9,10 @@ final _remoteMovieServiceProvider = Provider<RemoteMovieService>((ref) {
   final dio = Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/3/movie'));
 
   return RemoteMovieService(dio: dio);
+});
+
+// Repository
+final remoteMovieRepositoryProvider = Provider((ref) {
+  final service = ref.read(_remoteMovieServiceProvider);
+  return RemoteMovieRepository(service);
 });
