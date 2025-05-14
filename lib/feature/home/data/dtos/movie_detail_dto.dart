6a8bf5008cc54d5edb130c80198cdba31782d804
Meezzi <alswl-3016@naver.com>
2025-05-14 17:08:54 +0900

    feat: ProductionCompany에 logoPath 및 originCountry 추가

--- lib/feature/home/data/dtos/movie_detail_dto.dart
+++ lib/feature/home/data/dtos/movie_detail_dto.dart
@@ -184,11 +184,23 @@ class Genres {
 class ProductionCompany {
   final int id;
   final String name;
+  final String logoPath;
+  final String originCountry;
 
-  ProductionCompany({required this.id, required this.name});
+  ProductionCompany({
+    required this.id,
+    required this.name,
+    required this.logoPath,
+    required this.originCountry,
+  });
 
   factory ProductionCompany.fromJson(Map<String, dynamic> json) {
-    return ProductionCompany(id: json['id'], name: json['name'] ?? '');
+    return ProductionCompany(
+      id: json['id'],
+      name: json['name'] ?? '',
+      logoPath: json['logo_path'] ?? '',
+      originCountry: json['origin_country'] ?? '',
+    );
   }
 
   Map<String, dynamic> toJson() => {'id': id, 'name': name};
